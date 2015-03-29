dec476bacf0e798f7a5045f4f387be8ee5a0aa0b
thuxl <thuxl07@gmail.com>
2015-03-29 16:11:04 +0800

    test0004 for sizeof

--- make
+++ make
@@ -1,2 +1,5 @@
 g++ -g sortedListToBST.cc -o psortedListToBST
 g++ test0001.cc -o ptest0001
+g++ test0002.cc -o ptest0002
+g++ test0003.cc -o ptest0003
+g++ test0004.cc -o ptest0004g++; gcc test0004.cc -o ptest0004gcc
---
+++ test0004.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/* for test sizeof */
+
+class A{public:
+	bool a;
+	int b;
+	bool c;
+};
+class B{public:
+	int a;
+	bool b;
+	bool c;
+};
+
+int main ()
+{
+	char a;
+	char b[]="123";
+	void *p;
+	double *pp;
+	
+	printf("\tsizeof(char)=%d,sizeof(a)=%d,sizeof(b)=%d\n", sizeof(char), sizeof(a), sizeof(b) );
+	printf("\tsizeof(int)=%d,sizeof(short)=%d,sizeof(long)=%d\n", sizeof(int), sizeof(short), sizeof(long) );
+	printf("\tsizeof(unsigned int)=%d,sizeof(bool)=%d,sizeof(unsigned long)=%d\n", 
+			sizeof(unsigned int), sizeof(bool), sizeof(unsigned long) );
+	printf("\tsizeof(long long)=%d,sizeof(void)=%d,sizeof(void *)=%d\n", sizeof(long long), sizeof(void), sizeof(p) );
+	printf("\tsizeof(float)=%d,sizeof(double)=%d,sizeof(long double)=%d\n", sizeof(float), sizeof(double), sizeof(long double) );
+	printf("\tsizeof(double *)=%d\n", sizeof(pp) );
+	printf("----\n\tsizeof(class A)=%d,sizeof(class B)=%d\n", sizeof(A), sizeof(B) );
+
+  return 0;
+}
+//g++ test0001.cc -o ptest0001
